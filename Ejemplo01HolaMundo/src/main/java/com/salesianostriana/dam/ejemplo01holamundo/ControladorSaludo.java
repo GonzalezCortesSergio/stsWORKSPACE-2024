b7b6c5adfb777e0590aaaa0ea83beba778c8d254
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-04-12 10:49:49 +0200

    Ejemplo01HolaMundo terminado

--- Ejemplo01HolaMundo/src/main/java/com/salesianostriana/dam/ejemplo01holamundo/ControladorSaludo.java
+++ Ejemplo01HolaMundo/src/main/java/com/salesianostriana/dam/ejemplo01holamundo/ControladorSaludo.java
@@ -1,7 +1,9 @@
 package com.salesianostriana.dam.ejemplo01holamundo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControladorSaludo {
