47d36592ce51a72812cea9b62864c2b1150b4245
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-04-16 14:07:30 +0200

    Intentando hacer cositas mediante los tutoriales de Thymeleaf

---
+++ EjemploMensajesThymeleaf/src/test/java/com/salesianostriana/dam/ejemplomensajesthymeleaf/EjemploMensajesThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplomensajesthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploMensajesThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
