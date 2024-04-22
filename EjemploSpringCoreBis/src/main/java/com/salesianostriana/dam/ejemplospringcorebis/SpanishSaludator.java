1de16dd26266d99cc42843da441721b08677ac06
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-04-22 12:59:53 +0200

    Toquecitos para comprender el PostConstructor y PreDestroy de Spring

--- EjemploSpringCoreBis/src/main/java/com/salesianostriana/dam/ejemplospringcorebis/SpanishSaludator.java
+++ EjemploSpringCoreBis/src/main/java/com/salesianostriana/dam/ejemplospringcorebis/SpanishSaludator.java
@@ -3,6 +3,9 @@ package com.salesianostriana.dam.ejemplospringcorebis;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Service
 @Primary
 public class SpanishSaludator implements Saludator{
@@ -12,6 +15,19 @@ public class SpanishSaludator implements Saludator{
 		// TODO Auto-generated method stub
 		
 		System.out.println("Muy wenas alumnos de 1º DAM, a ver si aprobamos el tercer trimestre");
+	}
+	
+	@PostConstruct
+	void alComenzar() {
+		
+		System.out.println("Método \"alComenzar\" anotado con @PostConstruct");
+	}
+	
+	@PreDestroy
+	void anteDeDestruir() {
+		
+		System.out.println("Método \"antesDeDestruir\" anotado con @PreDestroy");
+		
 	}
 
 	
