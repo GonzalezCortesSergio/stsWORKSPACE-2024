660eb008229a72970e47f978ef79bc78d9db4c4e
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-05-03 14:29:22 +0200

    Cositas

--- EjemploCrud_SergioGonzalezCortes/src/main/java/com/salesianostriana/dam/ejemplocrudsergiogonzalezcortes/repository/RepositorioAlumno.java
+++ EjemploCrud_SergioGonzalezCortes/src/main/java/com/salesianostriana/dam/ejemplocrudsergiogonzalezcortes/repository/RepositorioAlumno.java
@@ -1,5 +1,9 @@
 package com.salesianostriana.dam.ejemplocrudsergiogonzalezcortes.repository;
 
-public interface RepositorioAlumno {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salesianostriana.dam.ejemplocrudsergiogonzalezcortes.model.Alumno;
+
+public interface RepositorioAlumno extends JpaRepository<Alumno, Long>{
 
 }
